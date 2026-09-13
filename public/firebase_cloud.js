fc76d54bfca6d5057bfa53b80f4e1690fe71bbb4
athulharidasvk-cpu <athulharidasvk@gmail.com>
2026-09-13 22:37:08 +0530

    Finish reference-style navigation

--- public/firebase_cloud.js
+++ public/firebase_cloud.js
@@ -3,3 +3,4 @@ import "./firebase_cloud_core.js";
 import "./game_navigation.js";
 import "./game_navigation_runtime_fix.js";
 import "./game_ui_reference_v2.js";
+import "./game_nav_reference_v2.js";

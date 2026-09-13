afec0f4647bdff07695b1dc65dc914b1583a7e81
athulharidasvk-cpu <athulharidasvk@gmail.com>
2026-09-13 22:56:57 +0530

    Load ATHUL FC game-feel v3 UI layer

--- public/firebase_cloud.js
+++ public/firebase_cloud.js
@@ -3,5 +3,6 @@ import "./firebase_cloud_core.js";
 import "./game_navigation.js";
 import "./game_navigation_runtime_fix.js";
 import "./game_ui_reference_v2.js";
+import "./game_ui_gamefeel_v3.css";
 import "./game_nav_reference_v2.js";
 import "./pls_2_5d_patch.js";

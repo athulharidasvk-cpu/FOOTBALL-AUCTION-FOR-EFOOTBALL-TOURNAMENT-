c86db0ad743c3c9ed47abd97c95d01804d802c79
athulharidasvk-cpu <athulharidasvk@gmail.com>
2026-09-13 22:36:30 +0530

    Apply large reference-style game UI

--- public/firebase_cloud.js
+++ public/firebase_cloud.js
@@ -1,4 +1,5 @@
-// Firebase entry point plus the non-destructive game navigation patch.
+// Firebase entry point plus the non-destructive game navigation/UI patches.
 import "./firebase_cloud_core.js";
 import "./game_navigation.js";
 import "./game_navigation_runtime_fix.js";
+import "./game_ui_reference_v2.css";

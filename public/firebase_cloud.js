59b616076f3f27455034e645410b8708e1d4714b
athulharidasvk-cpu <athulharidasvk@gmail.com>
2026-09-13 22:31:01 +0530

    Finalize game-style navigation compatibility patch

--- public/firebase_cloud.js
+++ public/firebase_cloud.js
@@ -1,4 +1,4 @@
 // Firebase entry point plus the non-destructive game navigation patch.
-// The original Firestore client lives in firebase_cloud_core.js.
 import "./firebase_cloud_core.js";
 import "./game_navigation.js";
+import "./game_navigation_runtime_fix.js";

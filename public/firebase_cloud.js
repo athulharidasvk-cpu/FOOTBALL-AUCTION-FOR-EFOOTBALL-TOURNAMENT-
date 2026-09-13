ee3ab4d930001d5f3bff98b9041355ec8a6deb14
athulharidasvk-cpu <athulharidasvk@gmail.com>
2026-09-13 22:36:41 +0530

    Fix UI stylesheet loading

--- public/firebase_cloud.js
+++ public/firebase_cloud.js
@@ -2,4 +2,4 @@
 import "./firebase_cloud_core.js";
 import "./game_navigation.js";
 import "./game_navigation_runtime_fix.js";
-import "./game_ui_reference_v2.css";
+import "./game_ui_reference_v2.js";
